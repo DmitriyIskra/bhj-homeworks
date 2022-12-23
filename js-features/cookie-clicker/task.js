2c7ccc545d9d444f1b2c92d0317bd83751982f61
Dmitriy Iskra <dmitriyiskra@mail.ru>
2022-12-24 00:06:28 +0300

    Complited task cookie-clicker stage2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,8 +1,11 @@
  const clickerCounter = document.querySelector('#clicker__counter');
+ const clickerSpeed = document.querySelector('#clicker__speed');
+ let lastClickTime = 0;
 
  const cookie = document.querySelector('.clicker__cookie');
 
  cookie.addEventListener('click', () => {
+    // Счетчик кликов
     clickerCounter.textContent = +clickerCounter.textContent + 1;
 
     if(cookie.width === 200) {
@@ -11,4 +14,8 @@
     else {
         cookie.width = 200;
     };
+
+    // Скорость клика
+    clickerSpeed.textContent = (1 / (Date.now()/1000 - lastClickTime)).toFixed(2);
+    lastClickTime = Date.now()/1000;
  });
