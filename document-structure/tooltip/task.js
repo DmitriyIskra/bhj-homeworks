0527a66e60aefe50e8012b61e5ba599cc6f724bd
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-01-10 20:05:17 +0300

    created task tooltip

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,32 @@
+const tooltip = document.querySelector('.tooltip');
+
+document.addEventListener('click', (e) => {
+    let valuetooltip;
+    let positionXClick;
+    let positionYClick;
+    let preActiveElement;
+
+    if(e.target.closest('.has-tooltip')) {
+        e.preventDefault();
+
+        if(preActiveElement) {
+            preActiveElement.classList.remove('tooltip_active');
+        };
+
+        preActiveElement = e.target;
+
+        valuetooltip = e.target.title;
+
+        positionXClick = e.target.getBoundingClientRect().left;
+        positionYClick = e.target.getBoundingClientRect().bottom;
+
+        tooltip.textContent = valuetooltip;
+        tooltip.setAttribute('style', `left: ${positionXClick}px; top: ${positionYClick + 7}px`);
+        tooltip.classList.add('tooltip_active');
+
+        console.log(tooltip)
+    }
+    else {
+        tooltip.classList.remove('tooltip_active');
+    };
+});
