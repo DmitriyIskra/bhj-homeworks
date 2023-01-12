00da8ef572e93bf880bd34a593e54ed01afbca28
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-01-12 19:40:08 +0300

    in file js add check tasksInput.value and trim()

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,24 +2,25 @@ const tasksInput = document.querySelector('.tasks__input');
 const tasksAdd = document.querySelector('.tasks__add');
 const tasksList = document.querySelector('.tasks__list');
 
-let inputValue;
-let pattern =  
+let inputValue;  
 
 tasksAdd.addEventListener('click', (e) => {
     e.preventDefault();
 
-    inputValue = tasksInput.value;
+    inputValue = tasksInput.value.trim();
 
-    tasksList.insertAdjacentHTML( 
-        'beforeend', `<div class="task">
-                        <div class="task__title">
-                            ${inputValue}
-                        </div>
-                        <a href="#" class="task__remove">&times;</a>
-                    </div>`
-     );   
-
-     inputValue = tasksInput.value = '';
+    if (inputValue) {
+        tasksList.insertAdjacentHTML( 
+            'beforeend', `<div class="task">
+                            <div class="task__title">
+                                ${inputValue}
+                            </div>
+                            <a href="#" class="task__remove">&times;</a>
+                        </div>`
+         );    
+    }; 
+    
+    tasksInput.value = '';
 });
 
 tasksList.addEventListener('click', (e) => {
@@ -27,4 +28,8 @@ tasksList.addEventListener('click', (e) => {
         e.target.closest('.task').remove()
     }
 })
+
+// * В первом задании, подсказку следует скрывать по повторному клику по ссылке, а не по странице. Так что, обработчик события по всему документу можно убрать.
+
+// * В третьем задании, добавление в корзину следует выполнять только по кнопке "Добавить в корзину"...и увеличение колества позиций в корзине так же должно выполняться только по кнопке добавления....Клик по кнопкам плюса и минуса должны менять количество только на витрине (карточке продукта).
 
