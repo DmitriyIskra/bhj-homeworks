7a41f64b5232c2853ea788ed9f375aab36dea9b0
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-01-16 20:33:28 +0300

    cmpleted task preloader standart version

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,35 @@
+const loader = document.querySelector('.loader');
+const itemsValutes = document.querySelector('#items');
+
+let responseValutesObject;
+
+
+xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
+
+xhr.send();
+
+xhr.addEventListener('readystatechange', e => {
+    if(xhr.readyState === xhr.DONE) {
+        loader.classList.remove('loader_active');
+        responseValutesObject = JSON.parse(xhr.responseText);
+         
+        for( let item in responseValutesObject.response.Valute) {
+            itemsValutes.insertAdjacentHTML('beforeend', `
+                <div class="item">
+                    <div class="item__code">
+                        ${responseValutesObject.response.Valute[item].CharCode}
+                    </div>
+                    <div class="item__value">
+                        ${responseValutesObject.response.Valute[item].Value}
+                    </div>
+                    <div class="item__currency">
+                        руб.
+                    </div>
+                </div>
+            `);
+        };
+    };
+});
+
