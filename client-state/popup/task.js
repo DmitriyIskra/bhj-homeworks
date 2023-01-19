0470bf40d602b8022c3de1d50862080f3f0b0b73
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-01-19 22:31:08 +0300

    completed task popup

---
+++ client-state/popup/task.js
@@ -0,0 +1,40 @@
+let resultCookie;
+
+const modal = document.querySelector('.modal');
+const modalClose = document.querySelector('.modal__close');
+
+modalClose.addEventListener('click', e => {
+    modal.classList.remove('modal_active');
+
+    setCookie('conditionModal', 'true')
+});
+
+
+
+function setCookie(name, value) {
+    document.cookie = `${name}=${encodeURIComponent(value)}`
+};
+
+function getCookie(name) {
+    const pairs = document.cookie.split('; ');
+    const findedCookie = pairs.find( el => el.startsWith(`${name}=`));
+    return findedCookie.substr(name.length + 1);
+};
+
+
+
+window.addEventListener('load', e => {
+    try {
+        resultCookie = getCookie('conditionModal');
+    }
+    catch(error) {
+        resultCookie = false;
+    };
+
+    if(resultCookie) {
+        modal.classList.remove('modal_active');
+    }
+    else{
+        modal.classList.add('modal_active');
+    };
+});
